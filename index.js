70e10acc0385846f1b2347cb3b14893cd7265045
Terence <zunmun@gmail.com>
2018-06-20 13:39:48 +0800

    Port Number for Heroku
    Change port number to try to work with Heroku

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.use(express.static('public'));
 var quiz = require('./RESTful_API.js');
 app.use('/RESTAPI', quiz);
 
-
-app.listen(3000);
-console.log("Running at localhost:3000");
+const port = process.env.PORT || 8080
+app.listen(8080);
+console.log("Running at localhost:8080");
