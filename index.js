2d05f5f1bb8aca532f86e8c6fe3e1f7be25e37f2
root <root@DESKTOP-61F9HO2.localdomain>
2021-06-14 14:42:37 +0800

    test

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var express = require('express');
+app.use(function(req, res, next) {
+    if (req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization')
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE')
+        if (req.method === 'OPTIONS') return res.send(200)
+    }
+    next()
+})
 var app = express();
 
 var morgan = require('morgan');
